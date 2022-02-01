42c0d597b4e56bfa6766f224f10494cdb2f16832
Emilly marques <emillymarques96622@gmail.com>
2022-02-01 14:28:20 -0300

    tarefa em execução

--- 6-salario.js
+++ 6-salario.js
@@ -1,12 +1,13 @@
-function salario(ouro,prata,cobre){
-let dia = 0
-for(dia *2 *ouro)
-if(){
+function salario(ouro, prata, cobre) {
+    let dia = 0
+    const ferreiro = cobre * (dia + 7) //10*7=70
+    const dev = prata * (dia + 5) //20*5=100
+    const merendeira = ouro * (dia + 10)//30*10= 300
+    return ferreiro + dev + merendeira
+
 
 }
-}
-
-salario(30,20,10)
+console.log(salario(30, 20, 10))
 
 /*salario = ouro,prata e cobre = numeros/parametros
 dias * ouro
