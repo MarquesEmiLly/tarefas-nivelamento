f97af49c8e5affa48a40c772f925cc563d710bc8
Emilly marques <emillymarques96622@gmail.com>
2022-02-03 15:15:08 -0300

    refatora 6 em andamento

--- 6-salario.js
+++ 6-salario.js
@@ -1,22 +1,57 @@
-function salario(profissao, dias) {
+function salario(moeda,profissao,dia){
+    switch () {
+        case valor1:
+          //Instruções executadas quando o resultado da expressão for igual á valor1
+          [break;]
+
+
+
+}
+console.log(salario(ouro,dev,5))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function salario(profissao,dias ) {
     let cobre = 10 //reais por dia 
     let prata = 20 //reais por dia
     let ouro = 30//reais por dia
     let profissoes = profissao.toUpperCase() //conversão das letras no console 
+    //let Ferreiro = 10 //dias trabalhado para cada profissional
     if (dias == 0) {
         return "Não trabalhou nenhum dia "
     }
     if (dias <= -1) {
         return "Esta devendo dias de trabalho"
     }
-    if (dias > 50) {     //caso ultrapassar os 50 dias
+    if (dias > 30) {     
         return "trabalhou mais do que devia!"
     }
-    if ("FERREIRO" === profissoes) {
+    if ("ferreiro" === profissoes) {
         const salario1 = cobre * dias //calculo do salario
         return `o salário do Ferreiro é de : ${salario1}`
+      //   return ferreiro ,cobre, "salario :" ferrreiro * cobre
     }
-    if ("ESCAVADOR" === profissoes) {
+    if (profissao === profissoes) {
         const salario2 = prata * dias //calculo do salario
         return `o salário do Escavador é de : ${salario2}`
     }
@@ -27,24 +62,14 @@ function salario(profissao, dias) {
     return "Profissão não reconhecida"
 }
 
-console.log(salario("mineirador", 0))
+console.log(salario("Mineirador", 20))
 
 
 
 
 
-
-
-/*salario = ouro,prata e cobre = numeros/parametros
-dias * ouro
-serviçais demoram Xdias para terminar os serviços 
-podemos ter mais de uma pessoa para cada serviço 
-calcule o salario deles 
-
-entrada:
-Função: Ferreiro
-Custo: 2 Cobres/dia
-
-saida:
-Salário: em 20 dias, deve se pagar a 3 ferreiros 60 cobres
 */
+
+
+
+
