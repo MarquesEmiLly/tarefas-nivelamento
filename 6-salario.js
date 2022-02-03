b3e1ab898693501ad73b3acee12b8016d314352d
willian <willian.chris.89@gmail.com>
2022-02-03 15:58:16 -0300

    exercício final com moedas em andamento

--- 6-salario.js
+++ 6-salario.js
@@ -1,75 +1,24 @@
-function salario(moeda,profissao,dia){
-    switch () {
-        case valor1:
-          //Instruções executadas quando o resultado da expressão for igual á valor1
-          [break;]
-
-
-
-}
-console.log(salario(ouro,dev,5))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-function salario(profissao,dias ) {
-    let cobre = 10 //reais por dia 
-    let prata = 20 //reais por dia
-    let ouro = 30//reais por dia
+function salario(profissao, dias, moedas) {
+    let moedaDeCobre = 10 //reais por dia 
+    let moedaDePrata = 20 //reais por dia
+    let moedaDeOuro = 30//reais por dia
     let profissoes = profissao.toUpperCase() //conversão das letras no console 
     //let Ferreiro = 10 //dias trabalhado para cada profissional
+    if (moedas <= "ouro") {
+        const calculo = dias * moedaDeOuro
+        return `${profissoes} trabalhou durante ${dias} e irá receber ${calculo} de ouro`
+
+    }
+
     if (dias == 0) {
         return "Não trabalhou nenhum dia "
     }
     if (dias <= -1) {
         return "Esta devendo dias de trabalho"
     }
-    if (dias > 30) {     
+    if (dias > 30) {
         return "trabalhou mais do que devia!"
     }
-    if ("ferreiro" === profissoes) {
-        const salario1 = cobre * dias //calculo do salario
-        return `o salário do Ferreiro é de : ${salario1}`
-      //   return ferreiro ,cobre, "salario :" ferrreiro * cobre
-    }
-    if (profissao === profissoes) {
-        const salario2 = prata * dias //calculo do salario
-        return `o salário do Escavador é de : ${salario2}`
-    }
-    if ("MINEIRADOR" === profissoes) {
-        const salario3 = ouro * dias //calculo do salario
-        return `O salário do Mineirador é de : ${salario3}`
-    }
-    return "Profissão não reconhecida"
 }
 
-console.log(salario("Mineirador", 20))
-
-
-
-
-
-*/
-
-
-
-
+console.log(salario("Mineirador", 20, "ouro"))
